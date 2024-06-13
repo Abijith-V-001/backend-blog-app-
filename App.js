f7e860f328518f11c81870fd9a704b481fbdb303
Abijith-V-001 <abivandiyil001@gmail.com>
2024-06-13 15:16:48 +0530

    token added successfully

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const { blogmodel } = require('./Models/Blog')
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const app = express()
 app.use(cors())
@@ -24,11 +25,39 @@ app.post("/signup", async (req, res) => {
     let Blog = new blogmodel(input)
     Blog.save()
 
-    res.send("Sign up for working")
+    res.json({ "status": "success" })
 })
 
+app.post("/signin", (req, res) => {
+    let input = req.body
+    blogmodel.find({ "email": req.body.email }).then(
+        (response) => {
 
+            if (response.length > 0) {
+                let dbpwd = response[0].pwd
+                console.log(dbpwd)
+                bcrypt.compare(input.pwd, dbpwd, (error, ismatch) => {
+                    if (ismatch) {
+                        jwt.sign({email:input.emailid},"blog-app",{expiresIn:"1d"},(error,token)=>{
+                            if (error) {
+                                res.json({"status":"unable to create token"})
+                            } else {
+                                res.json({"status":"Logged in","userId":response[0]._id,"token":token})
+                            }
+                        })
+                    } else {
+                        res.json({ "status": "Password Incorrect" })
+                    }
+                }
+                )
 
-app.listen(8084, () => {
+            } else {
+                res.json({ "status": "Entered email id is not registered" })
+            }
+        }
+    ).catch()
+})
+
+app.listen(8083, () => {
     console.log("Server Running")
 })
